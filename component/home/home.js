ceb775138f2d524d6b2fa7a9b0f4c6909d4ae305
zhangsiwei <821039040@qq.com>
2017-06-29 21:45:47 +0800

    'home完成'

--- component/home/home.js
+++ component/home/home.js
@@ -19,8 +19,13 @@ define(['uiRouter'],function  () {
         }])
         .controller("homeCtrl",["$scope",'$http','getData',function ($scope,$http,getData) {
 
-            getData.get('component/home/json/fruit.json').then(function (res) {
-                console.log(res);
+            getData.get('component/home/json/home.json').then(function (res) {
+                // console.log(res.data.data);
+                $scope.homeGoods=res.data.data.groupList[2].dataList;
+            })
+            getData.get('component/home/json/home2.json').then(function (res2) {
+                // console.log(res2);
+                $scope.homeGoods2=res2.data.data.groupList["0"].dataList;
             })
         }])
 
