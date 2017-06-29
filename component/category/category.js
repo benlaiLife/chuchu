8f7a6a95f83f8bf47b0292aeb4c3b58d4ff870f9
zhangyingyu <329939703@qq.com>
2017-06-30 00:12:12 +0800

    zyyfirstcategory

--- component/category/category.js
+++ component/category/category.js
@@ -8,9 +8,19 @@ define(['uiRouter'],function  () {
             .state("category",{
                 url:"/category",
                 templateUrl:"component/category/category.html",
-                controller:"categoryCtrl"
+                controller:"categoryCtrl",
+                css:"component/category/category.css"
             })
     })
-    .controller("categoryCtrl",["$scope",function ($scope) {
+    .service('getData',['$http',function($http){
+    		this.get = function(){
+			return $http.get('component/category/category_woman.json');
+		}
+    }]) 
+    .controller("categoryCtrl",["$scope",'getData',function ($scope,getData) {
+    	  getData.get().then(function(res){
+    	  	console.log(res);
+        	$scope.arr = res.data.data;
+        }) 
     }])
 })
