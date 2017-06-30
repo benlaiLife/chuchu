3ebb51f8126ccc2c7414bd77047f39b3faf142ef
zhangsiwei <821039040@qq.com>
2017-06-30 09:34:27 +0800

    'home改bug'

--- component/home/home.js
+++ component/home/home.js
@@ -12,19 +12,19 @@ define(['uiRouter'],function  () {
                     css:'component/home/home.css'
                 })
         })
-        .service('getData',['$http',function ($http) {
+        .service('getHomeData',['$http',function ($http) {
             this.get=function (data) {
                 return $http.get(data);
             }
         }])
-        .controller("homeCtrl",["$scope",'$http','getData',function ($scope,$http,getData) {
+        .controller("homeCtrl",["$scope",'$http','getHomeData',function ($scope,$http,getData) {
 
             getData.get('component/home/json/home.json').then(function (res) {
-                // console.log(res.data.data);
+//                 console.log(res.data.data);
                 $scope.homeGoods=res.data.data.groupList[2].dataList;
             })
             getData.get('component/home/json/home2.json').then(function (res2) {
-                // console.log(res2);
+//                 console.log(res2);
                 $scope.homeGoods2=res2.data.data.groupList["0"].dataList;
             })
         }])
