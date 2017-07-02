95b06144ee4c4b93a16dba1904c1dc041a428b2f
zhangsiwei <821039040@qq.com>
2017-07-02 15:49:57 +0800

    '搜索完成'

--- component/search/search.js
+++ component/search/search.js
@@ -11,27 +11,69 @@ define(['uiRouter'], function() {
 		})
 
 		.controller("searchCtrl", ["$scope", '$http', function($scope, $http) {
-
+			$scope.orderStyle='';
+			$scope.keyword=true;
+			$scope.searchRes=false;
+			$scope.name='';
+			$scope.itemSearch=function  (item) {
+				$scope.searchVal=item;
+				$scope.keyword=false;
+				$scope.searchRes=true;
+				$scope.name=item;
+				var url = "https://api.chuchujie.com/api/?client={%22ageGroup%22:%22QD_web_webkit%22,%22channel%22:%22QD_web_webkit%22,%22deviceId%22:%220%22,%22gender%22:%221%22,%22imei%22:%220%22,%22packageName%22:%22com.culiu.purchase%22,%22platform%22:%22wap%22,%22sessionId%22:%220%22,%22shopToken%22:%220%22,%22userId%22:%220%22,%22version%22:%221.0%22,%22xingeToken%22:%22%22}&query={%22module%22:%22search%22,%22function%22:%22keyword%22,%22q%22:%22" + $scope.name + "%22,%22page%22:%221%22}&sort=favcount_desc"
+				$http.get(url).success(function  (res) {
+				$scope.obj = res.data.productList;
+			})
+			}
 			$scope.back = function() {
 				window.history.go(-1);
 			}
-			var name = '美';
-			if(chkstrlen(name)) {
-				name = toUnicode(name);
+			$scope.search=function () {
+				$scope.itemSearch($scope.sear);
 			}
-			var str = 'https://api.chuchujie.com/api/?client={"ageGroup":"QD_web_webkit","channel":"QD_web_webkit","deviceId":"0","gender":"1","imei":"0","packageName":"com.culiu.purchase","platform":"wap","sessionId":"0","shopToken":"0","userId":"0","version":"1.0","xingeToken":""}&query={"module":"search","function":"keyword","q":"美丽","page":"1"}'
-//			var url = "https://api.chuchujie.com/api/?client={%22ageGroup%22:%22QD_web_webkit%22,%22channel%22:%22QD_web_webkit%22,%22deviceId%22:%220%22,%22gender%22:%221%22,%22imei%22:%220%22,%22packageName%22:%22com.culiu.purchase%22,%22platform%22:%22wap%22,%22sessionId%22:%220%22,%22shopToken%22:%220%22,%22userId%22:%220%22,%22version%22:%221.0%22,%22xingeToken%22:%22%22}&query={%22module%22:%22search%22,%22function%22:%22keyword%22,%22q%22:%22" + name + "%22,%22page%22:%221%22}&sort=favcount_desc"
-			$http.get(str).then(function  (res) {
-				console.log(res.data);
-				$scope.obj = res.data.data.productList;
+			$scope.state=[true,false,false,false];
+			$scope.sortGoods=function (index) {
+				for (var i = 0; i < $scope.state.length; i++) {
+					if(i==index){
+						$scope.state[i]=true;
+					}
+					else{
+						$scope.state[i]=false;
+					}
+				}
+			}
+			if(chkstrlen($scope.name)) {
+				$scope.name = toUnicode($scope.name);
+			}
+//			搜索关键字
+			$http.get('component/search/json/searchKey.json').success(function  (res) {
+				$scope.searctItems=res.data.bannerGroupList["0"].bannerList;
 			})
+			
 		}])
+	
 })
 
 //将汉字转化成16进制
-function toUnicode(str) {　　　　 var val = "";　　　　 for(var i = 0; i < str.length; i++) {　　　　　　 if(val == "")　　　　　　　　 val = str.charCodeAt(i).toString(16);　　　　　　
-		else　　　　　　　　 val += "," + str.charCodeAt(i).toString(16);　　　　 }　　　　 return val;　　 }
+function toUnicode(str) {　　　　
+	var val = "";　　　　
+	for(var i = 0; i < str.length; i++) {　　　　　　
+		if(val == ""){
+			val = str.charCodeAt(i).toString(16);　
+		}　　　
+		else{
+			val += "," + str.charCodeAt(i).toString(16);　
+		}
+			 　　　
+	}　　　　
+	return val;　　
+}
 //判断是不是汉字
-function chkstrlen(str)　　 {　　　　 var strlen = 0;　　　　 for(var i = 0; i < str.length; i++)　　　　 {　　　　　　 if(str.charCodeAt(i) > 255) //如果是汉字，则字符串长度加2
+function chkstrlen(str)　　 {　　　　
+	var strlen = 0;　　　　
+	for(var i = 0; i < str.length; i++)　　　　 {　　　　　　
+		if(str.charCodeAt(i) > 255) //如果是汉字，则字符串长度加2
 			　　　　　　　　 return true;　　　　　　
-		else　　　　　　　　 return false;　　　　 }　　 }
+		else　　　　　　　　 return false;　　　　
+	}　　
+}
--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ define(['uiRouter','9yuan9','home','mine','category','search','css','lazyLoad','
 	var app = angular.module('myModule',['ui.router','angularCSS','9yuan9Module','homeModule','searchModule','mineModule','categoryModule','me-lazyload',"detailsModule","evaluateModule","recommendModule"])
 		.config(function($stateProvider,$urlRouterProvider){
 			$urlRouterProvider.otherwise('/home');
+			
 		})
-	;
 });
