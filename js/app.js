8305378ea7799999a17e324fcb0be596652d1e79
zhangyingyu <329939703@qq.com>
2017-07-03 17:06:06 +0800

    分类跳转搜索页

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-define(['uiRouter','9yuan9','home','mine','category','search','classfiy','css','lazyLoad','swiper',"details","evaluate","recommend"],function(){
-	var app = angular.module('myModule',['ui.router','angularCSS','9yuan9Module','homeModule','searchModule','mineModule','categoryModule','classfiyModule','me-lazyload',"detailsModule","evaluateModule","recommendModule"])
+define(['uiRouter','9yuan9','shopCar','home','mine','category','classfiy','search','css','lazyLoad','swiper',"details","evaluate","recommend"],function(){
+	var app = angular.module('myModule',['ui.router','angularCSS','9yuan9Module','homeModule','searchModule','mineModule','categoryModule','classfiyModule','me-lazyload',"detailsModule","evaluateModule","recommendModule","shopCarModule"])
 		.config(function($stateProvider,$urlRouterProvider){
 			$urlRouterProvider.otherwise('/home');
+			
 		})
-	;
 });
