0004f49d3d58dc840e6f9ddf66273cd3a9e798d4
zhangsiwei <821039040@qq.com>
2017-07-03 09:53:25 +0800

    '添加购物车模块'

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-define(['uiRouter','9yuan9','home','mine','category','search','css','lazyLoad','swiper',"details","evaluate","recommend"],function(){
-	var app = angular.module('myModule',['ui.router','angularCSS','9yuan9Module','homeModule','searchModule','mineModule','categoryModule','me-lazyload',"detailsModule","evaluateModule","recommendModule"])
+define(['uiRouter','9yuan9','shopCar','home','mine','category','search','css','lazyLoad','swiper',"details","evaluate","recommend"],function(){
+	var app = angular.module('myModule',['ui.router','angularCSS','9yuan9Module','homeModule','searchModule','mineModule','categoryModule','me-lazyload',"detailsModule","evaluateModule","recommendModule","shopCarModule"])
 		.config(function($stateProvider,$urlRouterProvider){
 			$urlRouterProvider.otherwise('/home');
 			
