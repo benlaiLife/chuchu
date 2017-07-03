25cabba7592e22a5c2f501af8886d0c8e274db1b
zhangsiwei <821039040@qq.com>
2017-07-03 17:49:05 +0800

    '购物车静态'

--- component/shopCar/shopCar.js
+++ component/shopCar/shopCar.js
@@ -5,8 +5,46 @@ define(['uiRouter'], function() {
 				.state("shopCar", {
 					url: "/shopCar",
 					templateUrl: "component/shopCar/shopCar.html",
-//					controller: "searchCtrl",
+					controller: "shopCarCtrl",
 					css: 'component/shopCar/shopCar.css'
 				})
 		})
-		})
+	.controller('shopCarCtrl',['$scope','$http',function ($scope,$http) {
+//		$http.
+		$scope.goodsChose=[false,false];
+		$scope.storeChose=false;
+//		商店选择
+		$scope.store=function () {
+			$scope.storeChose=!$scope.storeChose;
+//			如果全选所有商品,则所有商品被选中
+			if($scope.storeChose==true){
+				for (var i = 0; i < $scope.goodsChose.length; i++) {
+				$scope.goodsChose[i]=true;
+			}
+			}else{
+				for (var j = 0; j < $scope.goodsChose.length; j++) {
+				$scope.goodsChose[j]=false;
+			}
+			}
+		}
+//		单个商品选择
+		$scope.storeItem=function(item) {
+			for (var i = 0; i < $scope.goodsChose.length; i++) {
+				if(item==i){
+					if($scope.goodsChose[i]==false){
+						$scope.goodsChose[i]=true;
+					}else{
+						$scope.goodsChose[i]=false;
+					}
+				}
+			}
+//			for (var j = 0; j < $scope.goodsChose.length; j++) {
+//				if($scope.goodsChose[i]==false){
+//					$scope.storeChose=false;
+//				}else{
+//					$scope.storeChose=true;
+//				}
+//			}
+		}
+	}])
+})
